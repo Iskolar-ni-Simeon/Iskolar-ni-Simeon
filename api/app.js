23075c9db9a00a435f3c8c033cca2a6e7f79f6e4
dwnppo <dwnppoalt@outlook.com>
2024-11-15 22:38:05 +0800

    adjustments to session cookie expiry, removal of unecessary logs

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const session = require('client-sessions');
 const crypto = require('crypto');
 
 const indexRouter = require('../routes/indexRouter.js');
@@ -9,10 +8,13 @@ const loginRouter = require('../routes/loginRouter.js');
 const thesisRouter = require('../routes/thesisRouter.js');
 
 const { authMiddleware } = require('../public/scripts/auth');
+const { SessionAuthentication } = require('../public/scripts/auth');
 
 const app = express();
 const PORT = 8080;
 const key1 = crypto.randomBytes(32).toString('hex');
+const sessAuth = new SessionAuthentication(process.env.SESSIONSECRET);
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
@@ -27,8 +29,9 @@ app.use("/", authMiddleware, indexRouter);
 app.use("/",  authMiddleware, thesisRouter);
 
 app.all('*', authMiddleware, (req, res) => {
+  const decryptedSession = sessAuth.decrypt(JSON.parse(Buffer.from(req.cookies.session, 'base64').toString('utf8')))
     res.status(404).render("./404.ejs", {
-        picture: req.session.picture,
+        picture: decryptedSession.picture,
         currentRoute: req.originalUrl,
     });
 });
